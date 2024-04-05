0203a298546b44cb9454b8922f9609db4673607e
Marcel Dechant <marcel.dechant@freenet.de>
2024-04-05 13:47:21 +0200

    level

--- js/classes/character.class.js
+++ js/classes/character.class.js
@@ -31,7 +31,7 @@ class Character extends MovableObject {
                 this.otherDirection = true;
             }
 
-
+            this.world.camera_x = -this.x;
 
         }, 1000 / 60);
 
--- js/classes/world.class.js
+++ js/classes/world.class.js
@@ -1,23 +1,13 @@
 class World {
     character = new Character();
-    enemis = [
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-    ];
-    clouds = [
-        new Cloud()
-    ];
-    backgroundObjects = [
-        new BackgroundObject('img/5_background/layers/air.png', 0),
-        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
-        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
-    ];
+    enemies = level1.enemies;
+    clouds = level1.clouds;
+    backgroundObjects = level1.backgroundObjects;
 
     canvas;
     ctx;
     keyboard;
+    camera_x = 0;
 
     constructor(canvas, keyboard) {
 
@@ -35,12 +25,13 @@ class World {
 
     draw() {
         this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+        this.ctx.translate(this.camera_x, 0);
 
         this.addObjectsToMap(this.backgroundObjects);
-        this.addToMap(this.character);
-        this.addObjectsToMap(this.enemis);
         this.addObjectsToMap(this.clouds);
-
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.enemies);
+        this.ctx.translate(-this.camera_x, 0);
 
         // Draw wird immer wieder aufgerufen
         let self = this;
@@ -60,12 +51,12 @@ class World {
             this.ctx.save();
             this.ctx.translate(mo.width, 0);
             this.ctx.scale(-1, 1);
-            mo.x= mo.x*-1;
+            mo.x = mo.x * -1;
         }
 
         this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
         if (mo.otherDirection) {
-            mo.x= mo.x*-1;
+            mo.x = mo.x * -1;
             this.ctx.restore();
         }
 
